bbd3975e72ce3e31795856448c944971c3e4d35a
Daan Beverdam <daanbeverdam@gmail.com>
2015-09-08 15:37:45 +0200

    Added sorting of commands

--- example/commands/help.py
+++ example/commands/help.py
@@ -8,7 +8,9 @@ class HelpCommand(Command):
         if self.arguments() == 'help':
             return {'message': self.usage}
         reply = self.dialogs['reply']
+        command_list = []
         for name in os.listdir(os.path.dirname(os.path.abspath(__file__))):
             if name.endswith('.py') and name != '__init__.py':
-                reply += '\n/' + name[:-3]
+                command_list.append('/' + name[:-3])
+        reply += '\n' + '\n'.join(map(str, command_list))
         return {'message': reply}
