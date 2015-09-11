ebae88de12947cc4d03d9940d266bdb97158a7a8
Daan Beverdam <daanbeverdam@gmail.com>
2015-09-11 14:32:08 +0200

    cleaned up code

--- example/commands/quote.py
+++ example/commands/quote.py
@@ -8,41 +8,56 @@ class QuoteCommand(Command):
     def reply(self):
         arguments = self.arguments()
         self.check_quote_store()
-        quotes = self.data['quote_store']
         if arguments == None:
-            random_name = random.choice(quotes.keys())
-            quote_list = quotes[random_name]
-            reply = random.choice(quote_list) + ' -' + random_name
+            reply = self.random_quote()
         else:
             tokens = arguments.split(' ')
             if len(tokens) > 1:
                 if ':' in arguments:
-                    name = arguments.split(':')[0].title()
-                    quote = '"' + arguments.split(':')[1].strip() + '"'
-                    try:
-                        quotes[name].append(quote)
-                        self.data['quote_store'] = quotes
-                    except:
-                        quotes[name] = [quote]
-                        self.data['quote_store'] = quotes
-                    reply = self.dialogs['quote_saved']
-                elif tokens[0].title() in quotes and tokens[1] == 'all':
-                    quote_list = []
-                    for quote in quotes[tokens[0].title()]:
-                        quote_list.append(quote)
-                    reply = ('\n'.join(map(str, quote_list))  + '\n -' +
-                             tokens[0].title())
+                    reply = self.save_quote()
+                elif tokens[0].title() in self.data['quote_store'] and tokens[1] == 'all':
+                    reply = self.all_quotes_by_name(tokens)
             elif len(tokens) == 1:
-                if tokens[0].title() in quotes:
-                    quote_list = quotes[tokens[0].title()]
-                    reply = random.choice(quote_list) + ' -' + tokens[0].title()
+                if tokens[0].title() in self.data['quote_store']:
+                    reply = self.random_quote_by_name(tokens)
                 if tokens[0] == 'all':
-                    quote_list = []
-                    for name in quotes:
-                        for quote in quotes[name]:
-                            quote_list.append(quote + ' -' + name)
-                    reply = '\n'.join(map(str, quote_list))
+                    reply = self.all_quotes(tokens)
         return {'message': reply}
+
+    def random_quote(self):
+        random_name = random.choice(self.data['quote_store'].keys())
+        quote_list = self.data['quote_store'][random_name]
+        return random.choice(quote_list) + ' -' + random_name
+
+    def random_quote_by_name(self, tokens):
+        quote_list = self.data['quote_store'][tokens[0].title()]
+        return random.choice(quote_list) + ' -' + tokens[0].title()
+
+    def save_quote(self):
+        name = self.arguments().split(':')[0].title()
+        quote = '"' + self.arguments().split(':')[1].strip() + '"'
+        quotes = self.data['quote_store']
+        try:
+            quotes[name].append(quote)
+            self.data['quote_store'] = quotes
+        except:
+            quotes[name] = [quote]
+            self.data['quote_store'] = quotes
+        return self.dialogs['quote_saved']
+
+    def all_quotes_by_name(self, tokens):
+        quote_list = []
+        for quote in self.data['quote_store'][tokens[0].title()]:
+            quote_list.append(quote)
+        return ('\n'.join(map(str, quote_list))  + '\n -' +
+                tokens[0].title())
+
+    def all_quotes(self, tokens):
+        quote_list = []
+        for name in self.data['quote_store']:
+            for quote in self.data['quote_store'][name]:
+                quote_list.append(quote + ' -' + name)
+        return '\n'.join(map(str, quote_list))
 
     def check_quote_store(self):
         try:
