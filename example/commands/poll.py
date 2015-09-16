910904079b7bf87e22e3aee6402a9074f11496b7
Daan Beverdam <daanbeverdam@gmail.com>
2015-09-16 12:41:49 +0200

    general improvements

--- example/commands/poll.py
+++ example/commands/poll.py
@@ -4,6 +4,17 @@ import shelve
 
 class PollCommand(Command):
 
+    def reply(self):
+        if not self.is_active():
+            return self.new_poll()
+        elif self.message.text in self.data['poll_options_dict']:
+            return self.store_answer()
+        elif self.message.text in self.meta_commands:
+            return self.handle_meta()
+        elif self.message.text.split(' ', 1)[0][1:] == self.name:
+            return {'message': self.dialogs['poll_already_active']}
+        return {'message': None}
+
     def new_poll(self):
         tokens = self.arguments().split('*')
         question = tokens[0].strip()
@@ -19,7 +30,7 @@ class PollCommand(Command):
         self.data['poll_starter'] = self.message.sender_id
         if question != '':
             self.data['poll_active'] = True
-            reply = question + '\n- ' + '\n- '.join(map(str, options))
+            reply = question + '\n- ' + '\n- '.join(options)
         return {'message': reply, 'keyboard': formatted_options,
                 'force_reply': True}
 
@@ -57,7 +68,7 @@ class PollCommand(Command):
     def poll_results(self):
         reply = self.dialogs['results'] % self.data['poll_question']
         for option, voters in self.data['poll_options_dict'].iteritems():
-            reply += ('\n- ' + option + ': ' + ', '.join(map(str, voters)) +
+            reply += ('\n- ' + option + ': ' + ', '.join(voters) +
                       ' (%d %s)' % (len(voters), self.dialogs[('vote'
                       if len(voters) == 1 else 'votes')]))
         return {'message': reply}
@@ -67,14 +78,3 @@ class PollCommand(Command):
             self.data['poll_active'] = False
             return {'message': self.dialogs['end_poll'], 'keyboard': None}
         return {'message': self.dialogs['not_owner']}
-
-    def reply(self):
-        if not self.is_active():
-            return self.new_poll()
-        elif self.message.text in self.data['poll_options_dict']:
-            return self.store_answer()
-        elif self.message.text in self.meta_commands:
-            return self.handle_meta()
-        elif self.message.text.split(' ', 1)[0][1:] == self.name:
-            return {'message': self.dialogs['poll_already_active']}
-        return {'message': None}
