164faf3ba7943b7b86b9b030440d1deb34931c96
Daan Beverdam <daanbeverdam@gmail.com>
2015-09-29 15:40:34 +0200

    now supports random comics

--- example/commands/xkcd.py
+++ example/commands/xkcd.py
@@ -1,6 +1,7 @@
 from pybot.command import Command
 import json
 import urllib
+import random
 
 
 class XKCDCommand(Command):
@@ -8,6 +9,13 @@ class XKCDCommand(Command):
     def reply(self):
         url = 'http://xkcd.com/info.0.json'
         content = json.loads(urllib.urlopen(url).read())
-        image_link = content['img']
-        xkcd_photo = self.get_image(image_link)
-        return {'photo': xkcd_photo}
+        if self.arguments == 'random' or self.arguments == 'r':
+            total_comics = content['num']
+            n = str(random.choice(range(1, total_comics)))
+            url = 'http://dynamic.xkcd.com/api-0/jsonp/comic/' + n
+            content = json.loads(urllib.urlopen(url).read())
+            image_link = content['img']
+        else:
+            image_link = content['img']
+        xkcd_image = self.get_image(image_link)
+        return {'photo': xkcd_image}
