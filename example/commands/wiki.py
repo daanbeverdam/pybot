baa129cf41611e91c8d3761ca83bc791632c20e3
Daan Beverdam <daanbeverdam@gmail.com>
2015-10-14 13:24:50 +0200

    added wiki link

--- example/commands/wiki.py
+++ example/commands/wiki.py
@@ -6,11 +6,8 @@ from pybot.command import Command
 class WikiCommand(Command):
 
     def reply(self):
-        if self.arguments == 'help':
-            return {'message': self.usage}
-        query = self.arguments
-        search_url = ("https://en.wikipedia.org/w/api.php?action=query"
-                      "&list=search&srsearch=" + query + "&utf8=&format=json")
+        search_url = ("https://en.wikipedia.org/w/api.php?action=query&list=se"
+                      "arch&srsearch=" + self.arguments + "&utf8=&format=json")
         search_results = json.loads(urllib.urlopen(search_url).read())
         if search_results['query']['searchinfo']['totalhits'] > 0:
             article_title = search_results['query']['search'][0]['title']
@@ -18,8 +15,10 @@ class WikiCommand(Command):
                            "&action=query&prop=extracts&exintro=&explaintext="
                            "&titles=" + article_title + "&exchars=500")
             article_contents = json.loads(urllib.urlopen(article_url).read())
+            wiki_url = ('https://en.wikipedia.org/wiki/' +
+                        article_title.replace(' ', '_'))
             extract = (article_contents['query']['pages']
                        [list(article_contents['query']['pages'])
-                       [0]]['extract'])
+                       [0]]['extract'] + '\n' + wiki_url)
             return {'message': extract}
-        return {'message': self.dialogs['no_results'] % query}
+        return {'message': self.dialogs['no_results'] % self.arguments}
