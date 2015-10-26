4ebc879cbaa73836a4eb47625e731bd90c3e1b49
Daan Beverdam <daanbeverdam@gmail.com>
2015-10-26 14:49:25 +0100

    some pep8 compliant changes

--- pybot/multipart.py
+++ pybot/multipart.py
@@ -6,6 +6,7 @@ import mimetypes
 import urlparse
 import uuid
 
+
 def post_multipart(url, fields, files):
     parts = urlparse.urlparse(url)
     scheme = parts[0]
@@ -41,7 +42,8 @@ def encode_multipart_formdata(fields, files):
         L.append(value)
     for (key, filename, value) in files:
         L.append('--' + LIMIT)
-        L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
+        L.append('Content-Disposition: form-data; name="%s"; filename="%s"' %
+                 (key, filename))
         L.append('Content-Type: %s' % get_content_type(filename))
         L.append('')
         L.append(value)
