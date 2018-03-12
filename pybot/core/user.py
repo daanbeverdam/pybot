a7eb5fd93caedd58fd612ca61ec4c8fcd4aedbe6
Daan Beverdam <daanbeverdam@gmail.com>
2018-03-12 13:31:10 +0100

    added getter for full name

--- pybot/core/user.py
+++ pybot/core/user.py
@@ -14,3 +14,11 @@ class User(object):
             self.last_name = last_name
             self.username = username
             self.is_bot = is_bot
+
+    def get_full_name(self):
+        """Returns full first and second name (if provided)."""
+        if self.first_name and self.last_name:
+            return self.first_name + ' ' + self.last_name
+        else:
+            return self.first_name
+
